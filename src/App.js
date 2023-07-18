0b306859d0f586f6fe6801842c98aa195bbcc426
CJ Coleman <CJ.Coleman@xanda.net>
2023-07-18 09:12:33 +0100

    initial navbar commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Navbar, Button, Link, Text, Card, Radio } from "@nextui-org/react";
+import { Layout } from "./Layout.js";
+import { AcmeLogo } from "./AcmeLogo.js";
+import { VariantsSelectorWrapper } from "./VariantsSelectorWrapper.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    const [variant, setVariant] = React.useState("static");
+
+    const variants = ["static", "floating", "sticky"];
+
+    return (
+        <Layout>
+            <Navbar isBordered variant={variant}>
+                <Navbar.Brand>
+                    <AcmeLogo />
+                    <Text b color="inherit" hideIn="xs">
+                        ACME
+                    </Text>
+                </Navbar.Brand>
+                <Navbar.Content hideIn="xs">
+                    <Navbar.Link href="#">Features</Navbar.Link>
+                    <Navbar.Link isActive href="#">
+                        Customers
+                    </Navbar.Link>
+                    <Navbar.Link href="#">Pricing</Navbar.Link>
+                    <Navbar.Link href="#">Company</Navbar.Link>
+                </Navbar.Content>
+                <Navbar.Content>
+                    <Navbar.Link color="inherit" href="#">
+                        Login
+                    </Navbar.Link>
+                    <Navbar.Item>
+                        <Button auto flat as={Link} href="#">
+                            Sign Up
+                        </Button>
+                    </Navbar.Item>
+                </Navbar.Content>
+            </Navbar>
+            <VariantsSelectorWrapper>
+                <Card css={{ maxW: "50%" }}>
+                    <Card.Body css={{ pt: "$8", px: "$8" }}>
+                        <Radio.Group
+                            defaultValue="default"
+                            label="Select variant"
+                            orientation="horizontal"
+                            size="sm"
+                            value={variant}
+                            onChange={setVariant}
+                        >
+                            {variants.map((variant) => (
+                                <Radio key={variant} value={variant}>
+                                    {variant}
+                                </Radio>
+                            ))}
+                        </Radio.Group>
+                    </Card.Body>
+                </Card>
+            </VariantsSelectorWrapper>
+        </Layout>
+    );
 }
-
-export default App;
