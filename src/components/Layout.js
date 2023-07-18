3bd06f2bd99fb112aa7f6561525a78f6761ae39c
CJ Coleman <CJ.Coleman@xanda.net>
2023-07-18 15:37:51 +0100

    changed layout to include content and titlecard

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,25 @@
 import { Content } from "./Content.js";
 import { Box } from "./Box.js";
+import TitleCard from "./titleCard.js";
+import "../style/layout.css";
 
 export const Layout = ({ children }) => (
     <Box
         css={{
             maxW: "100%",
+            overflow: "hidden", // Add this line
         }}
     >
         {children}
-        <Content />
+
+        <div class="pages">
+            <section class="page-1">
+                <TitleCard />
+            </section>
+
+            <section class="page-2">
+                <Content />
+            </section>
+        </div>
     </Box>
 );
