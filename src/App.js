770b1e6e525b452898c2a66f615b092305bc957f
CJ Coleman <CJ.Coleman@xanda.net>
2023-07-18 09:20:49 +0100

    other type of navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
 import React from "react";
-import { Navbar, Button, Link, Text, Card, Radio } from "@nextui-org/react";
+import {
+    Navbar,
+    Button,
+    Link,
+    Text,
+    Card,
+    Spacer,
+    Radio,
+    useTheme,
+} from "@nextui-org/react";
 import { Layout } from "./Layout.js";
 import { AcmeLogo } from "./AcmeLogo.js";
 import { VariantsSelectorWrapper } from "./VariantsSelectorWrapper.js";
 
 export default function App() {
-    const [variant, setVariant] = React.useState("static");
+    const [variant, setVariant] = React.useState("default");
+    const [activeColor, setActiveColor] = React.useState("primary");
 
-    const variants = ["static", "floating", "sticky"];
+    const { isDark } = useTheme();
+
+    const variants = [
+        "default",
+        "highlight",
+        "highlight-solid",
+        "underline",
+        "highlight-rounded",
+        "highlight-solid-rounded",
+        "underline-rounded",
+    ];
+
+    const colors = ["primary", "secondary", "success", "warning", "error"];
 
     return (
         <Layout>
-            <Navbar isBordered variant={variant}>
+            <Navbar isBordered={isDark} variant="sticky">
                 <Navbar.Brand>
                     <AcmeLogo />
                     <Text b color="inherit" hideIn="xs">
                         ACME
                     </Text>
                 </Navbar.Brand>
-                <Navbar.Content hideIn="xs">
+                <Navbar.Content
+                    activeColor={activeColor}
+                    hideIn="xs"
+                    variant={variant}
+                >
                     <Navbar.Link href="#">Features</Navbar.Link>
                     <Navbar.Link isActive href="#">
                         Customers
@@ -31,26 +57,57 @@ export default function App() {
                         Login
                     </Navbar.Link>
                     <Navbar.Item>
-                        <Button auto flat as={Link} href="#">
+                        <Button
+                            auto
+                            flat
+                            as={Link}
+                            color={activeColor}
+                            href="#"
+                        >
                             Sign Up
                         </Button>
                     </Navbar.Item>
                 </Navbar.Content>
             </Navbar>
             <VariantsSelectorWrapper>
-                <Card css={{ maxW: "50%" }}>
-                    <Card.Body css={{ pt: "$8", px: "$8" }}>
+                <Card css={{ px: "$6", maxW: "90%" }}>
+                    <Card.Body>
                         <Radio.Group
                             defaultValue="default"
-                            label="Select variant"
+                            label="Select active variant"
                             orientation="horizontal"
                             size="sm"
                             value={variant}
                             onChange={setVariant}
                         >
                             {variants.map((variant) => (
-                                <Radio key={variant} value={variant}>
+                                <Radio
+                                    key={variant}
+                                    color={activeColor}
+                                    value={variant}
+                                >
                                     {variant}
+                                </Radio>
+                            ))}
+                        </Radio.Group>
+                        <Spacer y={0.5} />
+                        <Radio.Group
+                            defaultValue="default"
+                            label="Select active color"
+                            orientation="horizontal"
+                            size="sm"
+                            value={activeColor}
+                            onChange={setActiveColor}
+                        >
+                            {colors.map((color) => (
+                                <Radio
+                                    key={color}
+                                    color={activeColor}
+                                    value={color}
+                                >
+                                    {color === "primary"
+                                        ? "primary (default)"
+                                        : color}
                                 </Radio>
                             ))}
                         </Radio.Group>
